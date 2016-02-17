c88e32ebb5e3df9126b35ea12487f968af1f3002
ck.oconnell@gmail.com <ck.oconnell@gmail.com>
2016-02-16 18:43:13 -0800

    Various updates in prep for dev review Feb 17

---
+++ page-social.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ Template Name: Social Widget Embed
+*/
+?>
+
+<?php get_header(); ?>
+
+<section class="pageContent socialEmbed">
+
+    <div class="gridWrap">
+
+        <!-- <h1>Testing</h1> -->
+
+        <iframe src="http://arcteryx.tbits.me?class=socialWidget"></iframe>
+
+    </div>
+
+</section>
+
+
+
+
+<div style="clear: both;"></div>
+
+
+<?php get_footer(); ?>
